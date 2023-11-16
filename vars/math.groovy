16a96086af7dc4af2f761b10bd309ba7f6566c7e
321ckmdevops <140068916+321ckmdevops@users.noreply.github.com>
2023-11-16 20:39:59 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -2,14 +2,17 @@ def sum(a,b)
 {
   echo "The result of ${a} and ${b} is ${a+b}"
 }
+
 def diff(a,b)
 {
   echo "The result of ${a} and ${b} is ${a-b}"
 }
+
 def mul(a,b)
 {
   echo "The result of ${a} and ${b} is ${a*b}"
 }
+
 def div(a,b)
 {
   echo "The result of ${a} and ${b} is ${a/b}"
