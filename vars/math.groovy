4466dde933a6e038bcc98bb9413c51b7716952fa
321ckmdevops <140068916+321ckmdevops@users.noreply.github.com>
2023-11-16 20:31:04 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+  echo "The result of ${a} and ${b} is ${a+b}"
+}
+def diff(a,b)
+{
+  echo "The result of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  echo "The result of ${a} and ${b} is ${a*b}"
+}
+def div(a,b)
+{
+  echo "The result of ${a} and ${b} is ${a/b}"
+}
